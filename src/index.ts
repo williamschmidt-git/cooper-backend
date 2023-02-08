0e17734e9e4ffa1a9a51226f41451990dc792ede
williamschmidt-git <9williamschmidt@gmail.com>
2023-02-08 20:43:52 -0300

    cors changes

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,7 @@ dotenv.config();
 const app: Express = express();
 app.use(cors({
   origin: '*',
+  methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH'],
 }));
 const port = process.env.PORT;
 
